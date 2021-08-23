98c6e86725df632587d02d4ecac69cb79f79572e
jdyawe <815765316@qq.com>
2021-08-23 14:50:44 +0800

    20210823 SMA EMA CCI

--- Indicators.py
+++ Indicators.py
@@ -15,31 +15,27 @@ class Indicator:
         ## 列：每一个bar的字段信息（例如：ohlc，volume etc.）
         self.OriginalDatas = initData
 
-<<<<<<< HEAD
-    def SMA(self, periods=12):
-=======
-    def sma(self, periods=12):
->>>>>>> 1230bc92a7825e8a9031a5c5768ef30edfffcc74
+    def SMA(self, periods=12, **kwargs):
         # 简单移动平均
         close = self.OriginalDatas.close
         ma = close.rolling_mean(window=periods, min_periods=2)
         return ma
 
-<<<<<<< HEAD
-    def EMA(self, periods=12):
-=======
-    def ema(self, periods=12):
->>>>>>> 1230bc92a7825e8a9031a5c5768ef30edfffcc74
+    def EMA(self, periods=12, **kwargs):
         # 加权移动平均
         # periods大致等于希望用于计算的数据量，12就是12个bar
         # https://pandas.pydata.org/pandas-docs/version/0.17.0/generated/pandas.ewma.html
-
-        ema = self.OriginalDatas.close.ewma(com=periods, min_periods=2)
+        if 'data' in kws.keys():
+            ema = kws['data'].close.ewma(com=periods, min_periods=2)
+        else:
+            ema = self.OriginalDatas.close.ewma(com=periods, min_periods=2)
         return ema
 
-    def CCI(self, periods=12):
+
+    def CCI(self, periods=12, **kwargs):
         # 顺势指标
         # <-100为超卖区间  >100为超买区间
+        # 要计算外部数据的CCI，传入kws，data:...
         # https://zhuanlan.zhihu.com/p/82604762
         alpha = 0.015
         tp = self.OriginalDatas.loc[:,['high', 'low', 'close']].mean(axis=1)
@@ -50,4 +46,12 @@ class Indicator:
         cci = (tp-matp)/(alpha*mdev)
         return cci
 
-
+    def MACD(self, long=26, short=12, periods=9, **kwargs):
+        # 平滑异同移动平均
+        # 利用中长期ema背离情况计算
+        # 返回值： dif dea macd
+        dif = self.EMA(short) - self.EMA(long)
+        temp = {'data':dif}
+        dea = self.EMA(periods, temp)
+        macd = 2*(dif - dea)
+        return dif, dea, macd
